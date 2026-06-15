7a7262ea0b2bd3e80c43c7f574c4b7c3ad69b32b
Anonymous <anonymous@localhost>
2026-06-15 12:10:48 +0800

    hw02: implement product, accumulate, summation_using_accumulate, make_repeater

--- HW/hw02/hw02.py
+++ HW/hw02/hw02.py
@@ -32,7 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
-
+    ans = 1
+    for i in range(1,n+1):
+        ans *= term(i) 
+    return ans
 
 def accumulate(fuse, start, n, term):
     """Return the result of fusing together the first n terms in a sequence 
@@ -54,7 +57,10 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
-
+    
+    for i in range(1,n+1):
+        start = fuse(start,term(i))
+    return start
 
 def summation_using_accumulate(n, term):
     """Returns the sum: term(1) + ... + term(n), using accumulate.
@@ -68,7 +74,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add,0,n,term)
 
 
 def product_using_accumulate(n, term):
@@ -100,4 +106,8 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
-
+    def func(start):
+        for _ in range(n):
+            start = f(start)
+        return start
+    return func
